7961341f67aeac1f1decf3ca182fde4ecbd1eab3
DouglasIde <douglasymide@gmail.com>
2024-07-10 12:38:13 -0300

    Build: First Commit

---
+++ src/main/java/com/generation/playstream/PlaystreamApplication.java
@@ -0,0 +1,13 @@
+package com.generation.playstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaystreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/playstream/PlaystreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.playstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
