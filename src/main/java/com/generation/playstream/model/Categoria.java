00292a3392451e5bc64d7ddc19299736cc90e094
DouglasIde <douglasymide@gmail.com>
2024-07-10 14:50:22 -0300

    Build: Adding the attributes to the model category class

--- src/main/java/com/generation/playstream/model/Categoria.java
+++ src/main/java/com/generation/playstream/model/Categoria.java
@@ -1,5 +1,50 @@
 package com.generation.playstream.model;
 
-public class Categoria {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo gênero é OBRIGATÓRIO!!")
+	private String genero;
+	
+	@Column(name = "jogador")
+	private int jogador;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getJogador() {
+		return jogador;
+	}
+
+	public void setJogador(int jogador) {
+		this.jogador = jogador;
+	}
+	
+	
 }
