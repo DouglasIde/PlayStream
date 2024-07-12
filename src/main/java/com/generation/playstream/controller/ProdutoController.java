9be2b47b7c038aa4f1ff4031bc66b6850deb3d13
DouglasIde <douglasymide@gmail.com>
2024-07-11 23:22:23 -0300

    Creating a method to check products by Id

--- src/main/java/com/generation/playstream/controller/ProdutoController.java
+++ src/main/java/com/generation/playstream/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package com.generation.playstream.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,11 @@ public class ProdutoController {
 		return ResponseEntity.ok(produtosRepository.findAll());
 	}
 	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id){
+		return produtosRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+				
+	}
 }
