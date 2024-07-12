1a31b42d1c4876048ac59d88df0587d65b39b65b
DouglasIde <douglasymide@gmail.com>
2024-07-11 23:16:32 -0300

    feat: Creating the method to consult all products

--- src/main/java/com/generation/playstream/controller/ProdutoController.java
+++ src/main/java/com/generation/playstream/controller/ProdutoController.java
@@ -1,12 +1,29 @@
 package com.generation.playstream.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.playstream.model.Produto;
+import com.generation.playstream.repository.IProdutoRepository;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 
+	@Autowired
+	private IProdutoRepository produtosRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(produtosRepository.findAll());
+	}
+	
+	
 }
