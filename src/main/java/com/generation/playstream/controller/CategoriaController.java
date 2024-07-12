f9f2245f3bf34f9cc62b348daeee21cb2c30a5c0
DouglasIde <douglasymide@gmail.com>
2024-07-12 00:35:57 -0300

    feat: Creating the method to register Categories

--- src/main/java/com/generation/playstream/controller/CategoriaController.java
+++ src/main/java/com/generation/playstream/controller/CategoriaController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.playstream.model.Categoria;
 import com.generation.playstream.repository.ICategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categorias")
@@ -37,5 +41,11 @@ public class CategoriaController {
 	@GetMapping("/genero/{genero}")
 	public ResponseEntity<List<Categoria>> getByGenero(@PathVariable String genero){
 		return ResponseEntity.ok(categoriaRepository.findAllByGeneroContainingIgnoreCase(genero));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(categoriaRepository.save(categoria));
 	}
 }
