e2e0011cace2b265a58a187d40db3d72e2776325
DouglasIde <douglasymide@gmail.com>
2024-07-12 00:13:57 -0300

    feat: Creating the method to delete games

--- src/main/java/com/generation/playstream/controller/ProdutoController.java
+++ src/main/java/com/generation/playstream/controller/ProdutoController.java
@@ -1,18 +1,22 @@
 package com.generation.playstream.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.playstream.model.Produto;
 import com.generation.playstream.repository.IProdutoRepository;
@@ -57,5 +61,15 @@ public class ProdutoController {
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
 						.body(produtosRepository.save(produto)))
 						.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> produto = produtosRepository.findById(id);
+		
+		if(produto.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		produtosRepository.deleteById(id);
 	}
 }
