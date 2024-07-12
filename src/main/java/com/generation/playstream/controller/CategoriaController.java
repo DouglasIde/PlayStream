a349b2bd8eba694bb05f7635e442d491a9b1a52d
DouglasIde <douglasymide@gmail.com>
2024-07-12 00:22:19 -0300

    feat: Creating the method to consult all categories

--- src/main/java/com/generation/playstream/controller/CategoriaController.java
+++ src/main/java/com/generation/playstream/controller/CategoriaController.java
@@ -1,5 +1,27 @@
 package com.generation.playstream.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.playstream.model.Categoria;
+import com.generation.playstream.repository.ICategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
+	@Autowired
+	private ICategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll(){
+		return ResponseEntity.ok(categoriaRepository.findAll());
+	}
 }
